ac04f649993623a89913611919efeb430e6de447
Akash Kumar <itsakash314@gmail.com>
2026-07-11 13:30:09 +0530

    Time: 91 ms (12.33%), Space: 147.8 MB (6.06%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
-    void dfs(int i, unordered_map<int, vector<int>> &adj, vector<bool> &vis, int &e, int &v){
-        if(vis[i]) return;
+    // void dfs(int i, unordered_map<int, vector<int>> &adj, vector<bool> &vis, int &e, int &v){
+    //     if(vis[i]) return;
+    //     vis[i] = true;
+    //     v++;
+    //     e += adj[i].size();
+    //     for(auto &neigh: adj[i])
+    //         if(!vis[neigh]){
+    //             dfs(neigh, adj, vis, e, v);
+    //         }
+    // }
+    void bfs(int i, unordered_map<int, vector<int>> &adj, vector<bool> &vis, int &e, int &v){
+        queue<int> q;
+        q.push(i);
         vis[i] = true;
-        v++;
-        e += adj[i].size();
-        for(auto &neigh: adj[i])
-            if(!vis[neigh]){
-                dfs(neigh, adj, vis, e, v);
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            v++;
+            e += adj[node].size();
+            for(auto &neigh : adj[node]){
+                if(!vis[neigh]){
+                    vis[neigh] = true;
+                    q.push(neigh);
+                }
             }
+        }
     }
     int countCompleteComponents(int n, vector<vector<int>>& edges) {
         unordered_map<int, vector<int>> adj;
-
         for(auto edge : edges){
             int u = edge[0];
             int v = edge[1];
@@ -27,7 +43,7 @@ public:
             int e = 0;
             int v = 0;
 
-            dfs(i, adj, vis, e, v);
+            bfs(i, adj, vis, e, v);
             if(v*(v-1) == e) ans++;
         }
         return ans;
