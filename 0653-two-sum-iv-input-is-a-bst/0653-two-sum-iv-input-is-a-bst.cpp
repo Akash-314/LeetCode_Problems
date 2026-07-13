3095746fa40f8e48172f9864cf8eb15c9e087ed7
Akash Kumar <itsakash314@gmail.com>
2026-07-13 15:11:55 +0530

    Time: 0 ms (100%), Space: 37.1 MB (82.35%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int> &in){
-        if(root == NULL) return;
+    void inorder(TreeNode* root, vector<int>& in) {
+        if (root == NULL)
+            return;
         inorder(root->left, in);
-        if(root != NULL) in.push_back(root->val);
+        if (root != NULL)
+            in.push_back(root->val);
         inorder(root->right, in);
     }
     bool findTarget(TreeNode* root, int k) {
-        if(!root || (!root->left && !root->right)) return false;
+        if (!root || (!root->left && !root->right))
+            return false;
         vector<int> in;
         in.clear();
         inorder(root, in);
-        int i = 0, j = in.size()-1;
-        while(i < j){
+        int i = 0, j = in.size() - 1;
+        while (i < j) {
             int sum = in[i] + in[j];
-            cout<<sum<<endl;
-            if(sum == k) return true;
-            if(sum > k) j--;
-            else if(sum < k) i++;
+            if (sum == k)
+                return true;
+            if (sum > k)
+                j--;
+            else if (sum < k)
+                i++;
         }
         return false;
     }
