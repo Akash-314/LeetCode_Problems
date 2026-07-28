63f94f49bac117d9f9b23bdd949cc17ddbb5318f
Akash Kumar <itsakash314@gmail.com>
2026-07-28 15:37:06 +0530

    Time: 2 ms (77.92%), Space: 93.1 MB (17.2%) - LeetHub

--- 2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.cpp
+++ 2012-sum-of-beauty-in-the-array/2012-sum-of-beauty-in-the-array.cpp
@@ -8,9 +8,7 @@ public:
         suf[n-1] = 1e6;
         for(int i = 1; i < n; i++){
             pre[i] = max(pre[i-1], nums[i-1]);
-        }
-        for(int i = n-2; i >= 0; i--){
-            suf[i] = min(suf[i+1], nums[i+1]);
+            suf[n-i-1]=min(suf[n-i],nums[n-i]);
         }
         long long ans = 0;
         for(int i = 1; i < n-1; i++){
