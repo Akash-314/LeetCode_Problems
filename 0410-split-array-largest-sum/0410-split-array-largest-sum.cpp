5dd2d0867b88986deb1ff74fd7a124aa38c4222d
Akash Kumar <itsakash314@gmail.com>
2026-08-21 19:39:07 +0530

    Time: 0 ms (100%), Space: 10.3 MB (97.71%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
 #define ll long long
-    int f(vector<int>& nums, int page) {
-        int st = 1;
+    int f(vector<int>& nums, int partitionResult) {
+        int noOfPartition = 1;
         ll res = 0;
         for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] + res <= page) {
+            if (nums[i] + res <= partitionResult) {
                 res += nums[i];
             } else {
-                st++;
+                noOfPartition++;
                 res = nums[i];
             }
         }
-        return st;
+        return noOfPartition;
     }
     int splitArray(vector<int>& nums, int k) {
         int n = nums.size();
