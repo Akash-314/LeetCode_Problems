2e230b47b96bd5705651336acb2911e2338318df
Akash Kumar <itsakash314@gmail.com>
2026-08-24 14:40:37 +0530

    Time: 108 ms (54.91%), Space: 95.4 MB (41%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.cpp
+++ 1872-stone-game-viii/1872-stone-game-viii.cpp
@@ -16,11 +16,11 @@ public:
         for (int i = 1; i < n; i++) {
             pre[i] = pre[i - 1] + stones[i];
         }
-        dp[n-1] = pre[n-1];
-        for(int i = n-2; i >= 0; i--){
-        int take = pre[i] - dp[i+1];
-        int skip = dp[i+1];
-        dp[i] = max(take, skip);
+        dp[n - 1] = pre[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            int take = pre[i] - dp[i + 1];
+            int skip = dp[i + 1];
+            dp[i] = max(take, skip);
         }
         return dp[1];
     }
