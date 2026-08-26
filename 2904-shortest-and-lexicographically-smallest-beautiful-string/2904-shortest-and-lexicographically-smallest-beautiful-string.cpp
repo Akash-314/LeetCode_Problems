aa30fd47b80b6fdf1041d13947ba73805a07eeaf
Akash Kumar <itsakash314@gmail.com>
2026-08-26 11:59:55 +0530

    Time: 21 ms (5.04%), Space: 14.6 MB (5.79%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -4,14 +4,15 @@ public:
         int n = s.size();
         vector<string> ans;
         int mn = INT_MAX;
-        for(int i = 0; i < n; i++){
-            unordered_map<char,int>mp;
+        for (int i = 0; i < n; i++) {
+            unordered_map<char, int> mp;
             string temp = "";
-            for(int j = i; j < n; j++){
+            for (int j = i; j < n; j++) {
                 mp[s[j]]++;
                 temp += s[j];
-                if(temp.size() > mn) break;
-                if(mp['1'] == k) {
+                if (temp.size() > mn)
+                    break;
+                if (mp['1'] == k) {
                     mn = min(mn, (int)temp.size());
                     ans.push_back(temp);
                     break;
@@ -19,9 +20,9 @@ public:
             }
         }
         sort(ans.begin(), ans.end());
-        // for(int i = 0; i < ans.size(); i++) cout<<ans[i]<<endl;
-        for(string str : ans){
-            if(str.size() == mn) return str;
+        for (string str : ans) {
+            if (str.size() == mn)
+                return str;
         }
         return {};
     }
