4ebb705b7786f9dd31ecf325f45cc480885f5bd5
Akash Kumar <itsakash314@gmail.com>
2026-08-30 08:55:50 +0530

    Sync Solution: combinations

---
+++ Accepted/combinations/Solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void f(int i, int& n, int k, vector<int>& temp) {
+        if (k == 0) {
+            ans.push_back(temp);
+            return;
+        }
+        if (i > n)
+            return;
+        temp.push_back(i);
+        f(i + 1, n, k - 1, temp);
+        temp.pop_back();
+        f(i + 1, n, k, temp);
+    }
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> temp;
+        f(1, n, k, temp);
+        return ans;
+    }
+};
