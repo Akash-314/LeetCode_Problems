b9f203a4bab0f7fb614031b98cd6162afe82c0fc
Akash Kumar <itsakash314@gmail.com>
2026-09-03 10:43:45 +0530

    Time: 0 ms (100%), Space: 165.8 MB (62.83%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -4,18 +4,14 @@ public:
         int n = nums1.size();
         bool isEven = true, flag = true;
         int mn = INT_MAX, mne = INT_MAX;
-        for (int i = 0; i < n; i++) {
-            if (nums1[i] % 2)
-                mn = min(nums1[i], mn);
-            if (nums1[i] % 2)
-                isEven = false;
-            if (nums1[i] % 2 == 0)
-                mne = min(mne, nums1[i]);
+        for(int i = 0; i < n; i++){
+            if(nums1[i] % 2) mn = min(nums1[i], mn);
+            if(nums1[i] % 2) isEven = false;
+            if(nums1[i] % 2 == 0) mne = min(mne, nums1[i]);
+            
         }
-        if (isEven)
-            return true;
-        if (mne > mn)
-            return true;
+        if(isEven) return true;
+        if(mne > mn) return true;
         return false;
     }
 };
