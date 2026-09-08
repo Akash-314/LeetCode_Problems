d3ff4b3da84d7bdfc2e4fe60a14c9c6f44b6ba4d
Akash Kumar <itsakash314@gmail.com>
2026-09-08 19:25:34 +0530

    Time: 3 ms (86.13%), Space: 12.2 MB (73.34%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    unordered_map<int,unordered_map<long long, int>> dp;
+    int dp[21][5002];
+    // unordered_map<int,unordered_map<long long, int>> dp;
     int f(vector<int>& nums, int t, int i) {
         if (i < 0)
             return t == 0;
-        if (dp.count(i) && dp[i].count(t))
-            return dp[i][t];
+        if (dp[i][t + 2000] != -1)
+            return dp[i][t + 2000];
         int way1 = f(nums, t + nums[i], i - 1);
         int way2 = f(nums, t - nums[i], i - 1);
 
-        return dp[i][t] = way1 + way2;
+        return dp[i][t + 2000] = way1 + way2;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
+        memset(dp, -1, sizeof(dp));
         return f(nums, target, n - 1);
     }
 };
