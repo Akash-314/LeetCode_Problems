cd91e753f035d244ac2526445d6a45410c67ddbc
Akash Kumar <itsakash314@gmail.com>
2026-09-10 14:52:13 +0530

    Time: 67 ms (37.17%), Space: 44 MB (26.6%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,10 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:
+    TreeNode* f(TreeNode* root, TreeNode* p, TreeNode* q, TreeNode* parent) {
+        if(root == NULL) return NULL;
+        if (root == p || root == q)
+            return root;
+        TreeNode* left = f(root->left, p, q, root);
+        TreeNode* right = f(root->right, p, q, root);
+         if (left && right)
+            return root;
+
+        if (left)
+            return left;
+
+        if (right)
+            return right;
+
+        return NULL;
+        
+    }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == NULL || root == p || root == q) return root;
-        TreeNode* left = lowestCommonAncestor(root->left, p, q);
-        TreeNode* right = lowestCommonAncestor(root->right, p, q);
-        if (left && right) return root;
-        return left ? left : right;
+        
+        return f(root, p, q, NULL);
+
     }
 };
