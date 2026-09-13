c009f3bda43565312c39588e0d7d2d12944309c7
Akash Kumar <itsakash314@gmail.com>
2026-09-13 16:45:55 +0530

    Time: 53 ms (52.92%), Space: 12.9 MB (86.1%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int n;
-    int cntOv(vector<vector<int>>& a, vector<vector<int>>& b, int row, int col) {
+    int countOverlap(vector<vector<int>>& a, vector<vector<int>>& b, int rowOffset, int colOffset) {
         int cnt = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                int bi = row + i;
-                int bj = col + j;
+                int bi = rowOffset + i;
+                int bj = colOffset + j;
                 if (bi < 0 || bi >= n || bj < 0 || bj >= n)
                     continue;
                 if (a[i][j] == 1 and b[bi][bj] == 1) {
@@ -18,12 +18,12 @@ public:
     }
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
         n = img1.size();
-        int ans = 0;
+        int maxOverlap = 0;
         for (int i = -n + 1; i < n; i++) {
             for (int j = -n + 1; j < n; j++) {
-                ans = max(ans, cntOv(img1, img2, i, j));
+                maxOverlap = max(maxOverlap, countOverlap(img1, img2, i, j));
             }
         }
-        return ans;
+        return maxOverlap;
     }
 };
